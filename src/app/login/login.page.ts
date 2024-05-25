a9041471330ecb1a14d92f025b59b7c7dbd6a0e9
Bastián Rabelo <ba.rabelo@duocuc.cl>
2024-05-25 04:23:33 -0400

    Add Login "Nombre:"

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import type { IonInput } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
+  // Access the input element
+  inputModel = '';
+
+  @ViewChild('ionInputEl', { static: true }) ionInputEl!: IonInput;
+
+  /*ngOnInit() { 
+
+  }*/
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  onInput(ev: Event) {
+    const value = (ev.target as HTMLInputElement).value;
 
+    // Removes non alphanumeric characters
+    const filteredValue = value.replace(/[^a-zA-Z0-9]+/g, '');
+
+    /**
+     * Update both the state variable and
+     * the component to keep them in sync.
+     */
+    this.ionInputEl.value = this.inputModel = filteredValue;
+  }
 }
