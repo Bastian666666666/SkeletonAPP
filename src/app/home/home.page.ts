4ccc3cad67606221005706620bee592913fd1e66
Bastián Rabelo <ba.rabelo@duocuc.cl>
2024-05-26 19:12:39 -0400

    Agregadas preguntas borrador home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,9 +6,18 @@ import { ActivatedRoute, Router } from '@angular/router'; // Importamos Activate
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage implements OnInit { // A esta clase existente agregamos la logica
+
+  // Variables para llamar e insertar desde Login
   usuarioRecibido: string = ''; // Creamos una variable para recibir el usuario
   contrasenaRecibida: string = ''; // Creamos una variable para recibir la contraseña
+
+  // Variables de la pagina Home
+  nombre: String = ''; // Creamos una variable para digitar el nombre
+  apellido: String = ''; // Creamos una variable para digitar el apellido
+  nivelEducacion: String = ''; // Creamos una variable para digitar la educacion
+  fechaNacimiento: String = ''; // Creamos una variable para digitar la fecha
 
   constructor(private activerouter: ActivatedRoute, private router: Router) {
     this.activerouter.queryParams.subscribe(params => {
